de5842d007b05afe1a1ec6ff2f7207eb2d13ff5d
inamdarminaz <i2inamdarminaz@gmail.com>
2020-08-14 21:27:00 +0530

    CardList component added

---
+++ src/CardList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { robots } from './robots';
+import Card from './Card';
+
+const CardList = () => {
+
+    const cardComponent = robots.map((user, i)=> {
+        return <Card id = {robots[i].id} name = {robots[i].name} email = {robots[i].email} />
+    })
+
+    return (
+        <div>
+            {cardComponent}
+        </div>
+    );
+}
+
+export default CardList;
