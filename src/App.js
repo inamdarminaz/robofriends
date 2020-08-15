cac711202b961d53d38dd3cc56fc5ae63c0df62f
inamdarminaz <i2inamdarminaz@gmail.com>
2020-08-15 15:38:56 +0530

    searchbox

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import {robots} from './robots'
+
+class App extends React.Component{
+
+    constructor (){
+        super()
+        this.state={
+            robots:robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+      }
+    
+      render() {
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot =>{
+          return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+        return !robots.length ?
+          <h1>Loading</h1> :
+          (
+            <div className='tc'>
+              <h1 className='f1'>RoboFriends</h1>
+              <SearchBox searchChange={this.onSearchChange}/>
+             
+                <CardList robots={filteredRobots} />
+              
+            </div>
+          );
+      }
+    }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList.js';
+import App from './App.js';
 import * as serviceWorker from './serviceWorker';
-import {robots} from './robots'
+
 import 'tachyons'
 ReactDOM.render(
-  <CardList robots = { robots }/>,
+  <App/>,
   document.getElementById('root')
 );
 
